ca7a93e9bf13df1e0ee221396d7b38ea1fe43325
Armin <arminpudic@yahoo.co.uk>
2017-12-23 18:14:35 +0000

    Import subcomponents
    Into the battle-view component.

--- src/components/battle-view/index.js
+++ src/components/battle-view/index.js
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import Battlefield from './battlefield';
+import Deck from './deck';
+import Grave from './grave';
+import Health from './health';
+import Resource from './resource';
 import './index.css';
 
 export default class BattleView extends React.PureComponent {
   render() {
     return (
       <div>
-        Battle View
+        <Battlefield/>
+        <Deck/>
+        <Grave/>
+        <Health/>
+        <Resource/>
       </div>
     )
   }
