4aa4893b4f670da00d0041312178c05a760fcc08
Armin <apudic@and.digital>
2018-01-08 14:48:08 +0000

    Add new menu items
    To main menu and style menu for better and more organised aesthetics. Also adds a logo image to the main menu.

---
+++ src/components/settings/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './index.css';
+
+export default class Settings extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Settings
+      </div>
+    )
+  }
+}
---
+++ src/components/training-view/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './index.css';
+
+export default class TrainingView extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        Training View
+      </div>
+    )
+  }
+}
