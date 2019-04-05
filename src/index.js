34b63fee93f147f3fc0007a8d2db64e2d7bb0324
Darth Arminius <arminpudic@yahoo.co.uk>
2019-04-05 11:17:22 +0100

    Add hot-loading via webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import { AppContainer } from "react-hot-loader";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById("root")
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./App.jsx", () => {
+    // eslint-disable-next-line global-require
+    const NextRootContainer = require("./App").default;
+    render(NextRootContainer);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["react-hot-loader/patch", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js"
