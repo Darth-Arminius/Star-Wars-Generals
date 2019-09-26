1f8970d89cda4a9cbf9a147ba7e503fae5105d4f
Darth Arminius <arminpudic@yahoo.co.uk>
2019-09-26 10:53:20 +0100

    Add card from JSON reader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: ["react-hot-loader/patch", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
   },
   module: {
     rules: [
@@ -13,23 +13,23 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader", "eslint-loader"]
+        use: ["babel-loader", "eslint-loader"],
       },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.(png|svg|jpg|gif)$/, use: "file-loader" }
-    ]
+      { test: /\.(png|svg|jpg|gif)$/, use: "file-loader" },
+    ],
   },
   mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html"
-    })
+      template: "public/index.html",
+    }),
   ],
   resolve: {
     extensions: [".js", ".json", ".jsx", ".png"],
     alias: {
       Components: path.resolve(__dirname, "src/components/"),
-      Assets: path.resolve(__dirname, "src/assets/")
-    }
-  }
+      Assets: path.resolve(__dirname, "src/assets/"),
+    },
+  },
 };
